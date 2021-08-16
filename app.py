a0865c349f655a9b4787b0d98389e8c0080407e6
Jannes Höke <mail@jhoeke.de>
2021-08-16 16:49:34 +0200

    🔥 Remove map endpoints

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 #!/user/bin/env python3.6
 
 import os
-from io import BytesIO
 from datetime import datetime, timezone
 
 import pytz
 from feedgen.feed import FeedGenerator
-from flask import Flask, Response, request, json, send_file, send_from_directory
+from flask import Flask, Response, request, json, send_from_directory
 
-from unwetter import db, generate, wina as wina_gen, slack, map, sentry
-from unwetter.config import SEVERITY_FILTER, STATES_FILTER, URGENCY_FILTER
+from unwetter import db, generate, wina as wina_gen, sentry
 from unwetter.generate import urls
 
 
@@ -50,139 +48,6 @@ def wina(id):
     r.headers["Content-Type"] = "text/xml; charset=iso-8859-1"
 
     return r
-
-
-def send_pil(img):
-    bio = BytesIO()
-    img.save(bio, "PNG")
-    bio.seek(0)
-
-    return send_file(bio, "image/png")
-
-
-@app.route("/map/<id>.png")
-def map_single(id):
-    img = map.generate_map([db.by_id(id)])
-    return send_pil(img)
-
-
-@app.route("/map")
-def map_multi():
-    ids = request.args.getlist("id")
-    disabled_ids = request.args.getlist("disabled")
-
-    mode = request.args.get("mode", "square")
-
-    title = request.args.get("title")
-    title_size = int(request.args.get("titleSize", 130))
-    subtitle = request.args.get("subtitle")
-    subtitle_size = int(request.args.get("subtitleSize", 110))
-
-    img = map.generate_map(
-        list(db.by_ids(ids)),
-        mode=map.Mode(mode),
-        disabled_events=list(db.by_ids(disabled_ids)),
-        title=title,
-        title_size=title_size,
-        subtitle=subtitle,
-        subtitle_size=subtitle_size,
-    )
-    return send_pil(img)
-
-
-@app.route("/basemap")
-def map_base_square():
-    img = map.generate_base_map(mode=map.Mode.SQUARE)
-    return send_pil(img)
-
-
-@app.route("/basemap_wide")
-def map_base_wide():
-    img = map.generate_base_map(mode=map.Mode.WIDE)
-    return send_pil(img)
-
-
-@app.route("/slack/event", methods=["GET", "POST"])
-def slack_event():
-    data = request.json or request.form
-
-    if not data:
-        return ""
-
-    if data.get("challenge"):
-        return data.get("challenge")
-
-    payload = data.get("payload")
-
-    if payload:
-        data = json.loads(payload)
-
-    print(data)
-
-    actions = data.get("actions")
-
-    for action in actions:
-        id = data["callback_id"]
-        channel_id = data["channel"]["id"]
-        user_id = data["user"]["id"]
-        event = db.by_id(id)
-        response = None
-
-        if action["name"] == "twitter":
-            response = "*Vorschlag Tweet*:\n" + generate.tweet(event)
-        elif action["name"] == "radio":
-            response = "*Vorschlag Radionachrichten*:\n" + generate.radio(event)
-        elif action["name"] == "crawl":
-            response = "*Vorschlag TV-Crawl:*\n" + generate.crawl(event)
-        elif action["name"] == "dwd":
-            response = "Offizielle Meldung des DWD:\n" + event["description"]
-        elif action["name"] == "info":
-            if "references" in event:
-                references = f'Referenzen: {", ".join(event["references"])}'
-            else:
-                references = ""
-
-            response = f"""
-Diese Meldung basiert auf offiziellen Informationen des Deutschen Wetterdienstes:
-https://www.dwd.de/DE/wetter/warnungen/warnWetter_node.html
-
-Die Bereitstellung dieser Information ist ein Projekt des Digitalen Wandels und wird aktiv weiterentwickelt.
-Informationen und Kontakt: {os.environ["WDR_PROJECT_INFO_URL"]}
-
-Event ID: {data["callback_id"]}
-
-{references}
-            """.strip()
-
-        if response:
-            slack.post_message(
-                response,
-                mrkdwn=True,
-                private=user_id,
-                channel=channel_id,
-                thread_ts=data["original_message"]["thread_ts"],
-            )
-
-    return ""
-
-
-@app.route("/slack/command/show", methods=["POST"])
-def slack_command_show():
-    data = request.form
-
-    if not data:
-        print("No Data :(")
-        return ""
-
-    event = db.by_id(data["text"])
-
-    if not event:
-        return f'No event found with ID {data["text"]}'
-
-    slack.post_message(f'Sending test event with ID {data["text"]}')
-    slack.post_event(event)
-
-    return f'Sent event with ID {data["text"]}'
 
 
 @app.route("/test")
