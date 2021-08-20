8a2df074517578f132362198a0407a8cd4297d97
Jannes Höke <mail@jhoeke.de>
2021-08-20 16:36:19 +0200

    🚀 Enable Sentry again

--- src/unwetter/sentry.py
+++ src/unwetter/sentry.py
@@ -4,9 +4,6 @@ import sentry_sdk
 
 
 def init():
-    # TODO: Turn back on when we have a proper Sentry setup
-    return
-
     sentry_sdk.init(
         os.environ.get("SENTRY_DSN"),
         release=os.environ.get("HEROKU_SLUG_COMMIT"),
